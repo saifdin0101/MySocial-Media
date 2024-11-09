8b68075a2ef20e9efb20cc0c57e23402552f92d7
saifdin0101 <saifdin808@gmail.com>
2024-11-09 19:33:09 +0100

    fixing modal  bug

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout >
     <div class="flex max-sm:flex-col relative ">
-        <div class="w-[20%] max-sm:w-full flex flex-col gap-5 justify-center items-center rounded-lg mt-[1rem] h-[40rem] bg-[#1c1c1d]">
+        <div class=" w-[20%] max-sm:w-full flex flex-col gap-5 justify-center items-center rounded-lg mt-[1rem] h-[40rem] bg-[#1c1c1d]">
             <button type="button "
                 class="w-[12rem] boton-elegante text-[#e2e5e9]  rounded-lg border-[1px] bg-transparent border-gray-700 py-2"
                 data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -31,6 +31,78 @@
                     <div class="font-semibold">Books</div>
                 </div>
             </div>
+            <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+        
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+        
+        
+                        <div class="modal-body ">
+        
+                            <button data-bs-dismiss="modal"
+                                class="text-2xl right-[36rem] top-[4.5rem]   z-30 absolute text-white">&#x2715;</button>
+                            <form enctype="multipart/form-data" action="{{ route('store.store') }}" method="post"
+                                class="border z-[29] absolute top-[4rem] right-[35rem] p-[3rem] border-black w-1/3 bg-[#1c1c1d] rounded-lg">
+                                @csrf
+        
+                                <h1 class="font-semibold text-2xl text-[#e2e5e9]">Create Ur Own Product :</h1>
+                                <div class="flex flex-col just checked: items-center gap-5">
+                                    <div class="mt-4">
+                                        <label for="file" class="custum-file-upload">
+                                            <div class="icon">
+                                                <svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg">
+                                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
+                                                        stroke-linejoin="round"></g>
+                                                    <g id="SVGRepo_iconCarrier">
+                                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                                            d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                                                            fill=""></path>
+                                                    </g>
+                                                </svg>
+                                            </div>
+                                            <div class="text">
+                                                <span>Click to upload image</span>
+                                            </div>
+                                            <input accept="images/*" name="image" id="file" type="file">
+                                        </label>
+        
+                                    </div>
+                                    <input class="w-[19rem] text-[#e2e5e9] rounded-lg border-2 bg-transparent border-gray-700"
+                                        name="title" placeholder="Title" type="text">
+                                    <input class="w-[19rem] text-[#e2e5e9] rounded-lg border-2 bg-transparent border-gray-700"
+                                        name="price" placeholder="Price" type="tel">
+                                    <select class="w-[19rem]  rounded-lg border-2 bg-transparent border-gray-700 text-[#646663]"
+                                        name="category" id="">
+                                        <option class="" disabled selected value="">Category</option>
+                                        <option value="fashion">fashion</option>
+                                        <option value="electronics">electronic</option>
+                                        <option value="House">House</option>
+                                        <option value="Book">Car</option>
+                                        <option value="bag">Book</option>
+        
+                                    </select>
+                                    <select class="w-[19rem] rounded-lg border-2 bg-transparent border-gray-700 text-[#646663]"
+                                        name="condition" id="">
+                                        <option class="" selected disabled value="">Condition</option>
+                                        <option value="new">New</option>
+                                        <option value="used">Used</option>
+                                    </select>
+        
+                                    <button data-bs-dismiss="modal"
+                                        class="w-[19rem] boton-elegante text-[#e2e5e9] rounded-lg border-2 bg-transparent border-gray-700 py-2">Publish
+                                        Ur Product</button>
+                                </div>
+        
+                            </form>
+                        </div>
+        
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4 w-[80%]" id="product-list">
             @foreach ($products as $product)
@@ -59,6 +131,9 @@
             </div>
             @endforeach
         </div>
+        
+        
+
     </div>
 
     <script>
@@ -86,4 +161,71 @@
             });
         });
     </script>
+    <!-- Modal -->
+
+
+
+
+
+
+
+
+
+
+
+
+    {{-- <div class="w-full "> --}}
+    {{-- <form enctype="multipart/form-data" action="" class="border p-[3rem] border-black w-1/3 bg-[#1c1c1d] rounded-lg">
+        <h1 class="font-semibold text-2xl text-[#e2e5e9]">Create Ur Own Product :</h1>
+        <div class="flex flex-col just checked: items-center gap-5">
+            <div class="mt-4">
+                <label for="file" class="custum-file-upload">
+                    <div class="icon">
+                        <svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                                    fill=""></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="text">
+                        <span>Click to upload image</span>
+                    </div>
+                    <input name="image" id="file" type="file">
+                </label>
+
+            </div>
+            <input class="w-[19rem] text-[#e2e5e9] rounded-lg border-2 bg-transparent border-gray-700" name="title"
+                placeholder="Title" type="text">
+            <input class="w-[19rem] text-[#e2e5e9] rounded-lg border-2 bg-transparent border-gray-700" name="price"
+                placeholder="Price" type="tel">
+            <select class="w-[19rem]  rounded-lg border-2 bg-transparent border-gray-700 text-[#646663]" name="Category"
+                id="">
+                <option class="" disabled selected value="">Category</option>
+                <option value="fashion">fashion</option>
+                <option value="electronics">electronics</option>
+                <option value="House">House</option>
+                <option value="Book">Book</option>
+                <option value="bag">bag</option>
+                <option value="">other...</option>
+            </select>
+            <select class="w-[19rem] rounded-lg border-2 bg-transparent border-gray-700 text-[#646663]" name="Condition"
+                id="">
+                <option class="" selected disabled value="">Condition</option>
+                <option value="">New</option>
+                <option value="">Used</option>
+            </select>
+            <button class="w-[19rem] boton-elegante text-[#e2e5e9] rounded-lg border-2 bg-transparent border-gray-700 py-2">Publish Ur Product</button>
+        </div>
+       
+    </form> --}}
+
+    <!-- Button trigger modal -->
+    {{-- 
+</div> --}}
+
+
 </x-app-layout>
